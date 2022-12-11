84f97d91a07460f0f9db40a59692c2efb5efae0c
Hamza ZOGHMAR <ha.zoghmar@gmail.com>
2022-12-11 19:04:39 +0100

    removed unused awesome themes

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -536,6 +536,7 @@ globalkeys = mytable.join(
     end,
     { description = "lua execute prompt", group = "awesome" }),
   --]]
+  --
   -- Custom Keybinding
   awful.key({ modkey }, "d", function()
     awful.util.spawn("rofi -modi drun,run -show drun -show-icons -theme ~/.config/polybar/shapes/scripts/rofi/launcher.rasi")
@@ -728,69 +729,6 @@ awful.rules.rules = {
 
 -- }}}
 
--- {{{ Signals
-
--- Signal function to execute when a new client appears.
-client.connect_signal("manage", function(c)
-  -- Set the windows at the slave,
-  -- i.e. put it at the end of others instead of setting it master.
-  -- if not awesome.startup then awful.client.setslave(c) end
-
-  if awesome.startup
-      and not c.size_hints.user_position
-      and not c.size_hints.program_position then
-    -- Prevent clients from being unreachable after screen count changes.
-    awful.placement.no_offscreen(c)
-  end
-end)
-
--- Add a titlebar if titlebars_enabled is set to true in the rules.
-client.connect_signal("request::titlebars", function(c)
-  -- Custom
-  if beautiful.titlebar_fun then
-    beautiful.titlebar_fun(c)
-    return
-  end
-
-  -- Default
-  -- buttons for the titlebar
-  local buttons = mytable.join(
-    awful.button({}, 1, function()
-      c:emit_signal("request::activate", "titlebar", { raise = true })
-      awful.mouse.client.move(c)
-    end),
-    awful.button({}, 3, function()
-      c:emit_signal("request::activate", "titlebar", { raise = true })
-      awful.mouse.client.resize(c)
-    end)
-  )
-
-  awful.titlebar(c, { size = 16 }):setup {
-    { -- Left
-      awful.titlebar.widget.iconwidget(c),
-      buttons = buttons,
-      layout  = wibox.layout.fixed.horizontal
-    },
-    { -- Middle
-      { -- Title
-        align  = "center",
-        widget = awful.titlebar.widget.titlewidget(c)
-      },
-      buttons = buttons,
-      layout  = wibox.layout.flex.horizontal
-    },
-    { -- Right
-      awful.titlebar.widget.floatingbutton(c),
-      awful.titlebar.widget.maximizedbutton(c),
-      awful.titlebar.widget.stickybutton(c),
-      awful.titlebar.widget.ontopbutton(c),
-      awful.titlebar.widget.closebutton(c),
-      layout = wibox.layout.fixed.horizontal()
-    },
-    layout = wibox.layout.align.horizontal
-  }
-end)
-
 -- Enable sloppy focus, so that focus follows mouse.
 client.connect_signal("mouse::enter", function(c)
   c:emit_signal("request::activate", "mouse_enter", { raise = vi_focus })
@@ -807,8 +745,3 @@ awful.spawn.with_shell("picom");
 awful.spawn.with_shell("xrandr --output HDMI-0 --left-of DP-4 --rotate right --auto");
 awful.spawn.with_shell("~/.config/polybar/shapes/launch.sh &");
 awful.spawn.with_shell("rw");
-
--- Keybindings
-
-awful.key({ modkey, }, "g", hotkeys_popup.show_help,
-  { description = "show help", group = "awesome" })
